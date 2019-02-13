2dbcb677698768f0a874695301825612236c8627
tomasz.jureczko <tomasz.jureczko@pwik.gliwice.pl>
2019-02-13 14:48:23 +0100

    dokumentacja

--- ArKorespV1/Models/PESKRZPOCZT.cs
+++ ArKorespV1/Models/PESKRZPOCZT.cs
@@ -13,18 +13,22 @@ namespace ArKorespV1.Models
     /// </summary>
     [CollectionType(ACollectionType.Document)]
     [CollectionName("PESKRZPOCZT")]
-    public class PESKRZPOCZT : CollectionMember, IDataRecord, IDictionaryAssignable
+    public class PESKRZPOCZT : AutoSignedCollectionMember, IDataRecord, IDictionaryAssignable
     {
         /// <summary>
-        /// collection's property 
+        /// collection's property - mailbox name
         /// </summary>
         [Display(Name ="Nazwa skrzynki")]
         public string DNAZWA { get; set; }
-        [Display(Name ="data modyfikacji")]
-        public DateTime? SDATA { get; set; }
+        /// <summary>
+        /// collection's property - mailbox short name
+        /// </summary>
         [Display(Name ="Symbol skrzynki")]
         [StringLength(50)]
         public string DSYMBOL { get; set; }
+        /// <summary>
+        /// collection's property - email associated
+        /// </summary>
         [Display(Name ="email skrzynki")]
         [StringLength(80)]
         public string DEMAIL { get; set; }
